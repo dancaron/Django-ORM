14be263b838f63f3540ce8266c4507e7d733239a
Dan Caron <dancaron@gmail.com>
2016-10-09 18:34:34 -0400

    Simplify comments

--- settings.py
+++ settings.py
@@ -5,9 +5,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATABASES = {
     'default': {
-        # 数据库驱动  Database driver
+        # Database driver
         'ENGINE': 'django.db.backends.sqlite3',
-        # 如果是mysql等数据库 这里应该是数据库名
         # Replace it with Database Name if use other DATABASES
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
@@ -17,6 +16,5 @@ INSTALLED_APPS = (
     'db',
 )
 
-# 强烈建议修改并 保护好它
-# SECURITY WARNING: keep the secret key used in production secret,recommended to modify it!!
+# SECURITY WARNING: Keep the secret key used in production secret, recommended to modify it!
 SECRET_KEY = '6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa'
