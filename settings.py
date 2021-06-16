68dfb594671dc92ccc6ec1deac3a2fa13cf80b3b
Dan Caron <dancaron@gmail.com>
2021-06-16 13:50:38 -0400

    Add DEFAULT_AUTO_FIELD for Django 3.2

--- settings.py
+++ settings.py
@@ -1,6 +1,5 @@
 import os
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -8,6 +7,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: Modify this secret key if using in production!
 SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
 
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 DATABASES = {
     "default": {
